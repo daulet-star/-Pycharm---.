7b024102c12f2288f30e2be592d2ea70eae0a9d1
daulet-star <dgochmetov@gmail.com>
2024-09-26 22:44:11 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+name='Python'
+total_tasks=12
+time_spent=1.5
+print(time_spent/total_tasks)
+print('Курс:'+name+' всего задач',total_tasks,'затрачено часо',time_spent,'ремя на одно задание',time_spent/total_tasks)
